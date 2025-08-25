6a22bc794a60dc689b7b94d056ac3921a3a21d33
nive19s <sothilingamnivethan19@gmail.com>
2025-08-26 00:27:43 +0530

    Navebar component, about , contact pages completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { currentUser } from "@clerk/nextjs/server"
+import Guest from "../components/Guest"
 
-export default function page() {
+export default async function HomePage() {
+  const user = await currentUser()
+
+if (!user) {
+  return <Guest/>
+}
+
   return (
-    <div className='text-yellow-500 bg-white'>Home Page
+    <div className='text-yellow-500 bg-white'>
+      <h1>Home Page</h1>
     </div>
   )
 }
