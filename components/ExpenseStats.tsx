fd31f3e6bf8827f41b8b44a3ef0e37fb7654d4c7
nive19s <sothilingamnivethan19@gmail.com>
2025-08-26 01:32:14 +0530

    Add Expense Record functionality added

---
+++ components/ExpenseStats.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ExpenseStats() {
+  return (
+    <div>
+      ExpenseStats
+    </div>
+  )
+}
---
+++ components/RecordHistory.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RecordHistory() {
+  return (
+    <div>
+      RecordHistory
+    </div>
+  )
+}
